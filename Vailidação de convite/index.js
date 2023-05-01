f15368e0a4a477f9c084140f86abd2621b5edf8a
Mayala Jesus <129312826+eumayala@users.noreply.github.com>
2023-05-01 13:09:56 -0300

    Update index.js

--- Vailidação de convite/index.js
+++ Vailidação de convite/index.js
@@ -9,26 +9,28 @@ btn.addEventListener('click', clicar);
 let names = ['RAFAEL', 'RICARDO'];
 let hosts = ['LUCAS', 'CAIO'];
 
-let isInput1Selected = false;
-let isInput2Selected = false;
 
 function clicar() {
     let input1up = input1.value.toUpperCase();
     let input2up = input2.value.toUpperCase();
+
+    if (!input1.value || !input2.value) {
+      alert('Preencha todos os campos!');
+      return;
+    }
   
     resultado.innerText = "";
     spinner.style.display = 'block'; // exibe o spinner
+
   
     setTimeout(function() {
+
       if (names.indexOf(input1up) !== -1 && hosts.indexOf(input2up) !== -1) {
         resultado.innerText = ("Acesso liberado para " + input1.value);
       } else {
         resultado.innerText = ("Não há registros de " + input1.value);
       }
 
-      isInput1Selected = true;
-      isInput2Selected = true;
-  
       spinner.style.display = 'none'; // remove o spinner
       
       // limpa as informações
@@ -37,7 +39,7 @@ function clicar() {
     
     }, 1000); // tempo em milissegundos que o spinner será exibido (3 segundos no exemplo)
 
-    if(isInput1Selected === false && isInput2Selected === false){
+    if(!isInput1Selected || !isInput2Selected){
       alert('Preencha todas as informações')
       resultado.innerText = "";
       return;
